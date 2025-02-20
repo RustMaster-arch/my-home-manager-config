8039e0d5e73cddf915b6f8f3513ca3db9570db7e
RustMaster-arch <rustmasterfullstack@gmail.com>
2025-02-20 12:50:52 -0500

    added dioxus cli

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@ in
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
   #
+
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
@@ -20,13 +21,16 @@ in
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
+
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
 
+    unstable.neovim
     # unstable.postman
     # unstable.bun
+    unstable.dioxus-cli
     unstable.zig
     unstable.atac
     unstable.gosec
